72d2be23066dc4b921c46092ff79d2c2ebe217ee
rgott421 <rgott@dobbinsco.com>
2026-05-04 15:34:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -252,7 +252,7 @@ async function saveToSharePoint() {
 
   const fileName = `Quote-${data.customer || 'Unknown'}-${Date.now()}.json`;
 
-  await fetch(https://defaultfd6501db952940d2bfb6372e714180.92.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/53b871a031904d778e5afc598438bfa2/triggers/manual/paths/invoke?api-version=1, {
+  await fetch("https://defaultfd6501db952940d2bfb6372e714180.92.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/53b871a031904d778e5afc598438bfa2/triggers/manual/paths/invoke?api-version=1", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
